8b4a723a5a5538954b67558bad1618179bdaad2b
bhaskar <newasebhaskar9@gmail.com>
2015-10-15 19:36:03 +0530

    angular node bridges

--- client/js/controllers/meetups-controller.js
+++ client/js/controllers/meetups-controller.js
@@ -1,4 +1,4 @@
-app.controller('meetupsController', ['$scope', '$resource', function ($scope, $resource){
+app.controller('MeetupCtrl', ['$scope', '$resource', function ($scope, $resource){
 
 	var Meetup = $resource('/api/meetups');
 	
@@ -11,8 +11,58 @@ app.controller('meetupsController', ['$scope', '$resource', function ($scope, $r
 		var meetup = new Meetup();
 		meetup.name = $scope.meetupName;
 		meetup.$save(function (result){
-			$scope.meetups.push(result);
-			$scope.meetupName = '';
+		$scope.meetups.push(result);
+		$scope.meetupName = '';
 		});
 	}
+
 }]);
+/**
+ * Controls the Blog
+ */
+app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
+  console.log("Blog Controller reporting for duty.");
+});
+
+/**
+ * Controls all other Pages
+ */
+app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+  console.log("Page Controller reporting for duty.");
+
+  // Activates the Carousel
+  $('.carousel').carousel({
+    interval: 5000
+  });
+
+  // Activates Tooltips for Social Links
+  $('.tooltip-social').tooltip({
+    selector: "a[data-toggle=tooltip]"
+  })
+});
+
+/**
+* Practice controllers
+*/
+
+app.controller('PracticeCtrl', function ($scope) {
+  $scope.saveData = function(){
+    var name = $scope.name;
+    var email = $scope.email;
+    var phone = $scope.phone;
+    var message = $scope.message;
+  }
+});
+
+app.controller('TablexCtrl', function ($scope){
+  var rowCollection = [];
+  $scope.rowCollection = [
+  {"firstName":"bhaskar", "lastName":"Newase", "birthDate":"June", "balance":5000, "email":"bhaskar@timelesslearntech.com" },
+  {"firstName":"anand", "lastName":"Newase", "birthDate":"June", "balance":5000, "email":"bhaskar@timelesslearntech.com" },
+  {"firstName":"rohit", "lastName":"Newase", "birthDate":"June", "balance":5000, "email":"bhaskar@timelesslearntech.com" },
+  {"firstName":"satish", "lastName":"Newase", "birthDate":"June", "balance":5000, "email":"bhaskar@timelesslearntech.com" },
+  {"firstName":"prasad", "lastName":"Newase", "birthDate":"June", "balance":5000, "email":"bhaskar@timelesslearntech.com" },
+  {"firstName":"yuvraj", "lastName":"Newase", "birthDate":"June", "balance":5000, "email":"bhaskar@timelesslearntech.com" }
+  ]
+
+});
