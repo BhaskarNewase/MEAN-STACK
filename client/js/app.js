6333f63a2257e7857c46eb4c5c65c56d10f3b869
bhaskar <newasebhaskar9@gmail.com>
2015-10-15 19:35:52 +0530

    angular node bridges

--- client/js/app.js
+++ client/js/app.js
@@ -1,2 +1,28 @@
-var app = angular.module('meetupApp', ['ngResource']);
-//var app = angular.module('meetupApp',[]);
+var app = angular.module('meetupApp', ['ngResource','ngRoute']);
+
+/**
+ * Main AngularJS Web Application
+ */
+
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    //practice pages
+    .when("/meetups", {templateUrl: "partials/meetups.html", controller: "MeetupCtrl"})
+    .when("/practice", {templateUrl: "partials/practice.html", controller: "PracticeCtrl"})
+
+    //table display
+    .when("/tablex", {templateUrl: "partials/tablex.html", controller: "TablexCtrl"})
+    // Pages
+    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+    .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
+    .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
+    .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
+    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+    // Blog
+    .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
+    .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
+    // else 404
+    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+}]);
