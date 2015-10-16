694b3973044e6cb47074aa8a1e24550e94189793
bhaskar <newasebhaskar9@gmail.com>
2015-10-16 19:15:25 +0530

    edit & delete

--- server/controllers/forms-controller.js
+++ server/controllers/forms-controller.js
@@ -6,3 +6,22 @@ module.exports.edit = function(req, res){
 		res.json(result);
 	});
 }
+
+module.exports.listdata = function(req, res){
+	User.find({}, function (err, results){
+		res.json(results);
+	});
+}
+
+module.exports.editrow = function(req, res){
+	User.find(function (err, results){
+		res.json(results);
+	});
+}
+
+module.exports.deleterow = function(req, res){
+	// User.find({}, function (err, results){
+	// 	res.json(results);
+	// });
+	//console.log('record deleted successfully');
+}
